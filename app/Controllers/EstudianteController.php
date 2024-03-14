b1dfa362c30690c24bc18eaea70b7861017bd1d6
Jesus Hernandez <37589925+hfebles@users.noreply.github.com>
2024-03-14 19:00:07 -0400

    update store studends

--- app/Controllers/EstudianteController.php
+++ app/Controllers/EstudianteController.php
@@ -4,39 +4,42 @@ namespace App\Controllers;
 
 use App\Models\Estudiante;
 use App\Controllers\Mantenimientos\MunicipiosController;
-class EstudianteController{
+
+class EstudianteController
+{
 
     private $estudiante;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->estudiante = new Estudiante();
-        
     }
 
-    public function index(){
+    public function index()
+    {
         $data = $this->estudiante->index();
         //require_once("app/views/estudiantes/index.php");
         require_once("public/views/estudiantes/index.php");
     }
 
-    public function create(){
+    public function create()
+    {
         $municipios = MunicipiosController::getMunicipios();
         $menciones = MencionController::getMenciones();
         require_once("app/views/estudiantes/create.php");
     }
 
-    public function store($request, $file){
+    public function store($request, $pdf)
+    {
         //$a = $this->estudiante->store($request);
         //header("Location: ".$_ENV['BASE_PATH']."/estudiante");
-        //var_dump(base64_encode(file_get_contents($file['name'])));
+        var_dump($pdf);
         die();
     }
 
-    public function delete(int $id){
+    public function delete(int $id)
+    {
         $this->estudiante->delete($id);
-        header("Location: ".$_ENV['BASE_PATH']."/estudiante");
+        header("Location: " . $_ENV['BASE_PATH'] . "/estudiante");
     }
-
 }
-
-?>
