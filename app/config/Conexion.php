c918ec4a64ca124b7a66cb4061750a2e1166a9e8
Jesus Hernandez <37589925+hfebles@users.noreply.github.com>
2024-03-19 18:04:54 -0400

    conexion

--- app/config/Conexion.php
+++ app/config/Conexion.php
@@ -1,8 +1,9 @@
 <?php
 
+
+
 namespace App\config;
 
-use Exception;
 use PDO;
 use PDOException;
 
@@ -23,7 +24,7 @@ class Conexion
             $this->conexion = new PDO("mysql:host={$this->servidor};dbname={$this->dbname}", $this->usuario, $this->password);
             $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die('Error en la conexión a la base de datos: ' . $e->getMessage());
+            return 'Error en la conexion: ' . $e;
         }
     }
 
@@ -39,64 +40,5 @@ class Conexion
         $sentencia = $this->conexion->prepare($sql);
         $sentencia->execute();
         return $sentencia->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-
-    public function backupDatabase($database)
-    {
-        try {
-
-            if (!file_exists('./' . $_ENV['PATH_BACKUP'])) {
-                mkdir('./' . $_ENV['PATH_BACKUP'], 0777, true);
-            }
-
-            $backup = fopen('./' . $_ENV['PATH_BACKUP'] . $database . "_" . date('Ymd') . "_" . date('His') . ".sql", 'w');
-            if (!$backup) {
-                throw new Exception("No se pudo abrir el archivo de respaldo para escribir.");
-            }
-
-            $dump = $this->conexion->prepare("SHOW CREATE DATABASE $database");
-            $dump->execute();
-            $databaseCreateStatement = $dump->fetchColumn(1);
-
-            fwrite($backup, $databaseCreateStatement . ";\n\n");
-
-            $tables = $this->conexion->query("SHOW TABLES");
-            while ($table = $tables->fetch(PDO::FETCH_NUM)) {
-                $tableName = $table[0];
-                $tableDump = $this->conexion->prepare("SHOW CREATE TABLE $tableName");
-                $tableDump->execute();
-                $tableCreateStatement = $tableDump->fetchColumn(1);
-                fwrite($backup, $tableCreateStatement . ";\n\n");
-
-                $rows = $this->conexion->query("SELECT * FROM $tableName");
-                while ($row = $rows->fetch(PDO::FETCH_ASSOC)) {
-                    $rowValues = array_map(function ($value) {
-                        return $this->conexion->quote($value);
-                    }, array_values($row));
-                    fwrite($backup, "INSERT INTO $tableName VALUES (" . implode(',', $rowValues) . ");\n");
-                }
-                fwrite($backup, "\n");
-            }
-
-            fclose($backup);
-
-            echo "Backup realizado correctamente. Archivo: " . $database . "_" . date('Ymd') . "_" . date('His') . ".sql \n";
-        } catch (Exception $e) {
-            die("Error al realizar el backup: " . $e->getMessage());
-        }
-    }
-
-    public function restoreDatabase($inputFile)
-    {
-        try {
-            $sql = file_get_contents('./' . $_ENV['PATH_BACKUP'] . $inputFile);
-
-            $this->conexion->exec($sql);
-
-            echo "Restauración realizada correctamente desde: $inputFile\n";
-        } catch (Exception $e) {
-            die("Error al restaurar desde el backup: " . $e->getMessage());
-        }
     }
 }
